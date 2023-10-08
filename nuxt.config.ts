a8098f2d57d8c83e84c69448651c15a803ad839d
TC Wang <wangtct@hotmail.com>
2023-10-08 15:08:11 +0800

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+
+  // gh-pages deploying
+  // target: "static",
+  // router: {
+  //   base: "/A7Kanban10/",
+  // },
+});
