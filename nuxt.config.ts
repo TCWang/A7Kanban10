588cb5b4480db261d45911bff8c8d611a7f6f27f
TC Wang <wangtct@hotmail.com>
2023-10-08 16:08:32 +0800

    Tailwind and NuxtImg

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,4 +7,26 @@ export default defineNuxtConfig({
   // router: {
   //   base: "/A7Kanban10/",
   // },
+
+  modules: ["@nuxtjs/tailwindcss", "@nuxt/image-edge"],
+
+  css: ["@/assets/css/tailwind.css"],
+
+  app: {
+    head: {
+      title: "Nuxt 3 Crash Course",
+      meta: [
+        {
+          name: "description",
+          content: "A7Kanban template base on Nuxt 3 Crash Course",
+        },
+      ],
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/icon?family=Material+Icons",
+        },
+      ],
+    },
+  },
 });
