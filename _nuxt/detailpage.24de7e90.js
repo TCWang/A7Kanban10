072a238ab0155e09a704808324a4a12cde86f82a
TC Wang <wangtct@hotmail.com>
2023-10-21 16:56:39 +0800

    Updates

--- _nuxt/detailpage.24de7e90.js
+++
@@ -1,1 +0,0 @@
-import{M as t,k as a,ab as r,j as n}from"./entry.39693965.js";const o={};function s(e,c){return n(),a("div",null,[r(e.$slots,"default")])}const d=t(o,[["render",s]]);export{d as default};
---
+++ _nuxt/detailpage.4b56b63f.js
@@ -0,0 +1,1 @@
+import{M as t,k as a,ab as r,j as n}from"./entry.981f790a.js";const o={};function s(e,c){return n(),a("div",null,[r(e.$slots,"default")])}const d=t(o,[["render",s]]);export{d as default};
