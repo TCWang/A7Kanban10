1e20e8db0672c45cc312802ef5e21dd0c556afc9
TC Wang <wangtct@hotmail.com>
2023-10-29 09:21:02 +0800

    Updates

---
+++ _nuxt/default.7767c72b.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./NavBar.6892fba3.js";import{A as a,c as n,b as c,a9 as s,o as r}from"./entry.69ed7a8e.js";const _={},d={class:"container mx-auto p-4"};function f(e,l){const o=t;return r(),n("div",d,[c(o),s(e.$slots,"default")])}const i=a(_,[["render",f]]);export{i as default};
---
+++ _nuxt/useShops.dee92ab1.js
@@ -0,0 +1,1 @@
+import{s}from"./shops.b4f4613e.js";const o=["0.A7重劃區","1.樂善國小","2.郵政物流","3.中心商業","4.文青國小","5.體育大學"],t=["食","衣","住","行","育","樂","所有"],e=()=>({shops:s,areas:o,typecs:t});export{e as u};
