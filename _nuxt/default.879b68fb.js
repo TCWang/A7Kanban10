a757d5e05c270d5333dbec48e3a08577a47f40ec
TC Wang <wangtct@hotmail.com>
2023-11-09 17:15:31 +0800

    Updates

--- _nuxt/default.879b68fb.js
+++
@@ -1,1 +0,0 @@
-import{_ as t}from"./NavBar.10e3a006.js";import{A as a,c as n,b as c,a9 as s,o as r}from"./entry.ef3eb264.js";const _={},d={class:"container mx-auto p-4"};function f(e,l){const o=t;return r(),n("div",d,[c(o),s(e.$slots,"default")])}const i=a(_,[["render",f]]);export{i as default};
