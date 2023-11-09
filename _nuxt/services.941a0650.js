56a2d2973fe59c036610434f14ece51f195fe686
TC Wang <wangtct@hotmail.com>
2023-11-09 15:25:26 +0800

    Updates

---
+++ _nuxt/services.941a0650.js
@@ -0,0 +1,37 @@
+const e=[{name:"竹風到府修理紗門窗網",id:"L001",type:"住",telphone:"0926-915-748",hours:"商家動態提供",figure:"LOC-001-01.jpeg",figureLogo:"LOC-001-03.jpeg",line:"https://line.me/ti/p/4nEIuWdAHy#~",category2:"A7",category:"0.A7重劃區",description:`報價方式: \r
+<ul>\r
+<li>請先加入LINE 好友: LINE ID: 0926915748</li>\r
+<li>將需維修的紗門/紗窗使用手機拍攝完整物件圖, 上傳Line 商家依據圖提供報價 </li>\r
+<li>與商家協商維修時間</li>\r
+<li>付款使用現金交付</li>\r
+</ul>\r
+ `},{name:"已悅美顏工作坊",id:"L002",type:"美",address:"桃園市龜山區文青路390號  美容師門口帶喔",figure:"LOC-002-01.jpeg",figureLogo:"LOC-002-02.jpeg",line:"https://lin.ee/boHjFsV",category2:"A7",category:"0.A7重劃區",description:`女性專寵 <br>\r
+粉刺痘痘深層清潔 60分鐘 $800, 客制化臉部課程110分鐘原價$1600 體驗$1250 <br>\r
+流程:卸妝〜洗~去角质~潤膚~清粉刺~精華導入~按摩~敷臉~頭、肩、頸放鬆~卸面膜~整膚~愉快回家<br>\r
+老師從事美業多年,對各種皮膚經驗豐富喔! 環境安全性高、溫馨,注重個人隱私<br>\r
+`,google:"https://www.google.com/maps/place/333%E6%A1%83%E5%9C%92%E5%B8%82%E9%BE%9C%E5%B1%B1%E5%8D%80%E6%96%87%E9%9D%92%E8%B7%AF390%E8%99%9F/@25.03903,121.3927495,17z/data=!3m1!4b1!4m5!3m4!1s0x3442a7717202873b:0xf4b3ecccd123198f!8m2!3d25.03903!4d121.3927495?authuser=0&hl=zh-TW&entry=ttu"},{name:"林口咪咪二手書坊",id:"L003",type:"育",address:"新北市林口區中山路590號一樓",telphone:"0933-307-747",hours:"全年無休，風雨無阻，10:00 - 21:00",figure:"LOC-003-01.jpeg",figureLogo:"LOC-003-02.png",facebook:"https://www.facebook.com/mimibook123",email:"a0933307747@gmail.com",suggestion:"https://www.youtube.com/watch?app=desktop&v=HIXn5TAVshc",suggestion2:"https://www.facebook.com/mimibook123/posts/pfbid08TwXmPCEm44p1DStrSYES9qKpzxVdxva5KY2VcKzpcvVZRAJ6it7QGfiVSrEy9vfl",category2:"A7",category:"0.A7重劃區",description:`1，因收書爆滿，造成顧客動線及找書不便，不得不，天天加班加点，汰舊換新，這几天，又直接丟棄5百多本書，來参觀的客人，覺得可惜，要我打折出售，我告訴他們，喜歡就隨便拿，不用客氣，一律免费。留下的書，就交给附近拾荒老人做公益。<br>\r
+2，過去經營那麽多年，受好多愛書人，惠賜眾多好書，歷經2次大病，又一直忙於工作，未克到府致謝，实在抱歉。在此懇求，如有需要的書籍，敬請不用客氣，歡迎蒞臨敍舊取書，聊表謝意，感恩。<br>`,google:"https://www.google.com/maps/place/%E5%92%AA%E5%92%AA%E4%BA%8C%E6%89%8B%E6%9B%B8%E5%9D%8A/@25.0857396,121.3734566,17z/data=!4m14!1m7!3m6!1s0x34681f0702e8ae3d:0x2526db0b2641ebe6!2z5ZKq5ZKq5LqM5omL5pu45Z2K!8m2!3d25.0857348!4d121.3760369!16s%2Fg%2F11bzq2xrx7!3m5!1s0x34681f0702e8ae3d:0x2526db0b2641ebe6!8m2!3d25.0857348!4d121.3760369!16s%2Fg%2F11bzq2xrx7?authuser=0&hl=zh-TW&entry=ttu"},{name:"上格窗簾",id:"L004",type:"住",telphone:"0933-638-736, 0975-307-636",figure:"LOC-004-01.jpeg",figureLogo:"LOC-004-02.png",web:"https://www.holkee.com/ba6bafd537fed2f51b37?fbclid=IwAR2ND8XWY3Hj3gplMeDGW7UskBiQ6evXhjinnMd84_uwmG03iSfxw1mVIFU",facebook:"https://www.facebook.com/care2015",line:" https://lin.ee/MZZTWhY",email:"innovation_care@yahoo.com.tw",category2:"A7",category:"0.A7重劃區",description:`<p>\r
+🏠龜山A7實體展示 (因平時都在外丈量安裝，目前皆採預約制，有興趣都可傳訊洽詢🙂) <br>\r
+預約免費丈量、規劃 享7.5折起優惠<br>\r
+🔖新建案廣告戶、團購戶歡迎洽詢，另享優惠折扣! <br>\r
+我們擁有20多年的工作經驗，工廠直銷，可以提供優惠的價格、優良的品質並量身訂做，提供專業的建議，為你居家生活加分<br>\r
+￡服務特色：\r
+<ul>\r
+<li>免費到府丈量、估價，提供各種專業建議。</li>\r
+<li>樣式豐富，國內→國外，平價→高級款式布料一應俱全。 </li>\r
+<li>產品如果有任何問題,均有售後服務。</li>\r
+<li>丈量安裝皆由我們本身一貫化處理 ，自身展示中心，省去承租 店面成本，回饋給顧客，歡迎預約參觀 </li>\r
+</ul>\r
+</p>\r
+`},{name:"林口Spotlight聚光蒸足",id:"L005",type:"樂",address:"桃園市龜山區復興二路131巷2號",telphone:"0935-565-060",hours:"10:00 - 19:30, 週三 13:00 - 19:00 週日09:00 - 19:00 週一公休",figure:"LOC-005-01.jpeg",figureLogo:"LOC-005-02.png",web:"https://spotlight-club.business.site/?m=true&fbclid=IwAR0h1LIxOn0F1m_62aX0fNagEmAVpikXPU-aBQxw_TbaKbWdKTYYBvp0fWI",facebook:"https://www.facebook.com/linkou18light",line:"https://lin.ee/lKpdbrT",ig:"https://www.instagram.com/linkou_18_spotlight/?fbclid=IwAR3vl054krK3JmjWNe05Y3qDQ0WOxF_RUBWYS6-z2Bvy7SDK9xZ6agmHDMk",suggestion:"https://www.youtube.com/watch?v=dhuxrtebwqM",category2:"A7",category:"0.A7重劃區",description:`蒸足後大汗淋灕流的不要不要🙅‍♀️黃金30分鐘攝取富含蛋白質的均衡飲食 <br>\r
+運動後食慾通常也增加🔝 如果沒有在飲食上做好妥善安排的話 隨手取得的便利食物攝取量絕對增加, 而這類的食物通常含有大量的單一碳水化合物和熱量真正達到氣血循環是運動+營養並進的💪💪💪\r
+根本調理讓健康從裡到外♻️`,google:"https://www.google.com/maps/place/%E6%9E%97%E5%8F%A3Spotlight%E8%81%9A%E5%85%89%E8%92%B8%E8%B6%B3/@25.0564728,121.3735101,17z/data=!4m15!1m8!3m7!1s0x3442a7045b22d9db:0xfacd7715397f906!2z5p6X5Y-jU3BvdGxpZ2h06IGa5YWJ6JK46Laz!8m2!3d25.0564728!4d121.3735101!10e2!16s%2Fg%2F11t7_c79cj!3m5!1s0x3442a7045b22d9db:0xfacd7715397f906!8m2!3d25.0564728!4d121.3735101!16s%2Fg%2F11t7_c79cj?authuser=0&hl=zh-TW&entry=ttu"},{name:"天鉅驗屋",id:"L006",type:"住",address:"桃園市龜山區樂善二路822號1樓",telphone:"03-328-6991",hours:"09:00 - 18:00 假日採預約制",figure:"LOC-006-01.jpeg",figureLogo:"LOC-006-02.png",web:"https://www.tienjuteam.com/%E6%9C%8D%E5%8B%99%E9%A0%85%E7%9B%AE?fbclid=IwAR296PA8nfPMYAAUtrqsw-50z-CyQ79SF6giBDGy5zJiXsGD6qNoPcXoGkE",facebook:"https://www.facebook.com/Tienjuteam/",line:" http://lin.ee/1y9M0Xs",description:`<p>\r
+💝天鉅為了您的幸福窩🏠，無論是新成屋或是中古屋，秉持以專業嚴謹的服務精神為您的房屋作公正、精準的檢測，提供最完整齊全的驗屋報告！\r
+天鉅始終為您心愛的房子好好把關❤️ 歡迎大家來店免費諮詢，了解更多關於驗屋的檢測資訊，或加入官方Line由專人為您的疑問一一解答\r
+</p>\r
+<ul>\r
+<li>專業相關證照</li>\r
+<li>專業教育訓練</li>\r
+<li>專業檢測儀器</li>\r
+<li>建立信心口碑</li>\r
+</ul>`,google:"https://www.google.com/maps/place/%E5%A4%A9%E9%89%85%E5%B7%A5%E7%A8%8B%E6%AA%A2%E9%A9%97%E6%9C%89%E9%99%90%E5%85%AC%E5%8F%B8%7C%E6%96%B0%E6%88%90%E5%B1%8B%E9%A9%97%E5%B1%8B%7C%E5%B0%88%E6%A5%AD%E9%A9%97%E5%B1%8B%7C%E6%96%B0%E5%8C%97%7C%E6%A1%83%E5%9C%92%7C%E6%9E%97%E5%8F%A3%7C%E9%BE%9C%E5%B1%B1/@25.0457983,121.3927412,17z/data=!3m1!4b1!4m6!3m5!1s0x3442a797a645c347:0x76bbafd8092e45dd!8m2!3d25.0457983!4d121.3927412!16s%2Fg%2F11scs6q5mz?entry=ttu"}];export{e as s};
--- _nuxt/useServices.186f542d.js
+++
@@ -1,1 +0,0 @@
-import{s}from"./services.45f1bc9e.js";import{a as r,t as e}from"./typecs.b72571d9.js";const o=()=>({services:s,areas:r,typecs:e});export{o as u};
